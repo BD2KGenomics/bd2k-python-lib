64dcfb9ef26fa1cd45a95cceca3aa38933947992
Hannes Schmidt <hannes@ucsc.edu>
2013-10-02 12:33:25 -0700

    Release 1.1 prep

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cghub-python-lib",
-    version="1.1.dev1",
+    version="1.1",
     packages=find_packages( ),
     install_requires=[ ],
     namespace_packages=[ 'cghub' ]
