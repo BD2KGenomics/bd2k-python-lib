74c78517ff3d509024524a8bf24da4bacddabf3a
Hannes Schmidt <hannes@ucsc.edu>
2013-10-02 11:40:26 -0700

    Renamed project to cghub-python-lib

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cghub-python-utils",
+    name="cghub-python-lib",
     version="1.1.dev1",
     packages=find_packages( ),
     install_requires=[ ],
