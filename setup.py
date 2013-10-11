f86efac1742a56f03548fed5e08039b62a3d604b
Hannes Schmidt <hannes@ucsc.edu>
2013-10-11 00:32:24 -0700

    Prepare next dev cycle

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cghub-python-lib",
-    version="1.2",
+    version="1.3.dev1",
     packages=find_packages( ),
     install_requires=[ ],
     namespace_packages=[ 'cghub' ]
