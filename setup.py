58fbd2c419e85102ae9a3031ef40fe7f3a2777bf
Hannes Schmidt <hannes@ucsc.edu>
2013-11-13 20:58:45 -0800

    Releasing 1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cghub-python-lib",
-    version="1.3.dev1",
+    version="1.3",
     packages=find_packages( ),
     install_requires=[ ],
     namespace_packages=[ 'cghub' ]
