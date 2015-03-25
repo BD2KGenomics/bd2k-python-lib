1673565c09a85cec907716082e927be406ae5345
Hannes Schmidt <hannes@ucsc.edu>
2015-03-24 19:17:01 -0700

    Added separate src directory

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="bd2k-python-lib",
     version="1.5.dev1",
-    packages=find_packages( ),
+    package_dir={ '': 'src' },
+    packages=find_packages( 'src' ),
     install_requires=[ ],
     namespace_packages=[ 'bd2k' ]
 )
