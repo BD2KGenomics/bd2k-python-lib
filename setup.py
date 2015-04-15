b424d189769fe4c8df8170fb2cf708ac909ca53f
Hannes Schmidt <hannes@ucsc.edu>
2015-04-14 19:44:11 -0700

    Added some more project info to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="bd2k-python-lib",
     version="1.5.dev1",
+
+    author='Hannes Schmidt',
+    author_email='hannes@ucsc.edu',
+    url='https://github.com/BD2KGenomics/bd2k-python-lib',
+    description='The BD2K Python module kitchen sink',
+
     package_dir={ '': 'src' },
     packages=find_packages( 'src' ),
     install_requires=[ ],
