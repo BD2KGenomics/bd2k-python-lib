b731e0b9abab32776325f4fb105ddca253e04409
CJ <cketchum@ucsc.edu>
2015-06-30 12:39:41 -0700

    Convert strings to more readable and natural english phrases. Includes tests.

--- src/bd2k/util/strings.py
+++ src/bd2k/util/strings.py
@@ -2,6 +2,31 @@
 
 import inspect
 
+def to_english(input_list, separator=",", conjuction="and"):
+    """
+    convert list to a more natural and readable string. If given empty list,
+    returns python's default empty string representation: [].
+
+    :param input_list: list of any type, as long as it can be cast to string
+    :param separator: char (or string) to insert between elements in list
+    :param conjuction: string used to connect the final element in list
+    :return: string
+
+    ex:
+    >>> to_english(['a','b','c'])
+    "a, b, and c "
+    """
+    if len(input_list)==0:
+        return str(input_list)
+    if len(input_list)==1:
+        return str(input_list[0])
+    if any(not isinstance(element, str) for element in input_list):
+        input_list = map(str, input_list)
+    last_element=input_list.pop()
+    separator+=" "
+    conjuction+=" "
+    english_list = separator.join(input_list)
+    return english_list+separator+conjuction+last_element
 
 def interpolate( template, skip_frames=0, **kwargs ):
     """
--- src/bd2k/util/test/test_strings.py
+++ src/bd2k/util/test/test_strings.py
@@ -1,6 +1,7 @@
 import unittest
 
 from bd2k.util.strings import interpolate
+from bd2k.util.strings import to_english
 
 foo = 4
 bar = 1
@@ -10,3 +11,10 @@ class TestStrings( unittest.TestCase ):
     def test_interpolate( self ):
         bar = 2  # should override the global foo
         self.assertEquals( interpolate( "{foo}{bar}" ), "42" )
+
+    def test_to_english( self ):
+        self.assertEqual("1, and 2", to_english([1,2]))
+        self.assertEqual("foo, and bar", to_english(['foo','bar']))
+        self.assertEqual("bar", to_english(['bar']))
+        self.assertEqual("1", to_english([1]))
+        self.assertEqual("[]", to_english([]))
