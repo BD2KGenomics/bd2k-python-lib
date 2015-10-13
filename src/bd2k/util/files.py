98f122dae07d7eddbb099f9855e7b951ca20f06f
Hannes Schmidt <hannes@ucsc.edu>
2015-10-13 00:27:29 -0700

    Added greedy read and write for Python 3.x file objects (DISABLED)

--- src/bd2k/util/files.py
+++ src/bd2k/util/files.py
@@ -14,4 +14,40 @@ def mkdir_p( path ):
         else:
             raise
 
+if False:
 
+    # These are not needed for Python 2.7 as Python's builtin file object's read() and write()
+    # method are greedy. For Python 3.x these may be useful.
+
+    def gread( readable, n ):
+        """
+        Greedy read. Read until readable is exhausted, and error occurs or the given number of bytes
+        have been read. If it returns fewer than the requested number bytes if and only if the end of
+        file has been reached.
+
+        :type readable: io.FileIO
+        """
+        bufs = [ ]
+        i = 0
+        while i < n:
+            buf = readable.read( n - i )
+            m = len( buf )
+            if m == 0:
+                break
+            bufs.append( buf )
+            i += m
+        return ''.join( bufs )
+
+
+    def gwrite( writable, buf ):
+        """
+        Greedy write. Write until the entire buffer has been written to or an error occurs.
+
+        :type writable: io.FileIO[str|bytearray]
+
+        :type buf: str|bytearray
+        """
+        n = len( buf )
+        i = 0
+        while i < n:
+            i += writable.write( buf[ i: ] )
---
+++ src/bd2k/util/test/test_files.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+from mock import MagicMock, call
+
+from bd2k.util.files import gread, gwrite
+
+
+class TestFiles( TestCase ):
+    if False:
+        # See comment in module under test
+        def test_gread( self ):
+            for n in range( 0, 4 ):
+                f = MagicMock( )
+                # The mock file contains "12". Each read() invocation shall return one byte from that,
+                # followed by the empty string for EOF.
+                f.read.side_effect = [ '1', '2', '' ]
+                # Read n bytes greedily
+                # noinspection PyTypeChecker
+                self.assertEqual( gread( f, n ), "12"[ :n ] )
+                # First call to read() should request n bytes and then one less on each subsequent call.
+                self.assertEqual( f.mock_calls, [ call.read( i ) for i in range( n, 0, -1 ) ] )
+
+        def test_gwrite( self ):
+            for n in range( 0, 3 ):
+                f = MagicMock( )
+                # Each write invocation shall write a single byte.
+                f.write.side_effect = [ 1 ] * n
+                s = "12"[ :n ]
+                # noinspection PyTypeChecker
+                gwrite( f, s )
+                # The first call to write() should be passed the entire string, minus one byte off
+                # the front for each subsequent call.
+                self.assertEqual( f.mock_calls, [ call.write( s[ i: ] ) for i in range( 0, n ) ] )
