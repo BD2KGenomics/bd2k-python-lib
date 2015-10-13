efcc761f9ddcd71cb38f8b26bff890971625b50d
Hannes Schmidt <hannes@ucsc.edu>
2015-10-13 00:29:51 -0700

    Added fixes to ExceptionalThread
    - maintain complete exception info instead of just the class
    - reraise in target thread *and* in joining thread
    - added doctest

--- src/bd2k/util/threading.py
+++ src/bd2k/util/threading.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
-
+import logging
+import sys
 import threading
-
 
 class BoundedEmptySemaphore( threading._BoundedSemaphore ):
     """
@@ -20,19 +20,29 @@ class ExceptionalThread( threading.Thread ):
     idempotent, the exception is only during the first invocation of join() that succesfully
     joined the thread. If join() times out, no exception will be re reraised even though an
     exception might already have occured in run().
+
+    >>> def f():
+    ...     assert False
+    >>> t = ExceptionalThread(target=f)
+    >>> t.start()
+    >>> t.join()
+    Traceback (most recent call last):
+    ...
+    AssertionError
     """
 
-    exception = None
+    exc_info = None
 
     def run( self ):
         try:
             super( ExceptionalThread, self ).run( )
-        except BaseException as e:
-            self.exception = e
+        except:
+            self.exc_info = sys.exc_info( )
             raise
 
-    def join( self, timeout=None ):
-        super( ExceptionalThread, self ).join( timeout )
-        if not self.is_alive and self.exception:
-            self.exception = None
-            raise self.exception
+    def join( self, *args, **kwargs ):
+        super( ExceptionalThread, self ).join( *args, **kwargs )
+        if not self.is_alive( ) and self.exc_info is not None:
+            type, value, traceback = self.exc_info
+            self.exc_info = None
+            raise type, value, traceback
