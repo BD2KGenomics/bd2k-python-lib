25e1be50f8a658c3c42c9e2cdc09379591d0203f
Hannes Schmidt <hannes@ucsc.edu>
2015-10-22 18:06:49 -0700

    Bump dev version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 kwargs = dict(
     name="bd2k-python-lib",
-    version="1.10.dev1",
+    version="1.10.dev2",
 
     author='Hannes Schmidt',
     author_email='hannes@ucsc.edu',
