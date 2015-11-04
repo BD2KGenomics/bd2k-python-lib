9b557e8643fba2b046906de28db437c8aad91eef
Hannes Schmidt <hannes@ucsc.edu>
2015-11-03 19:00:54 -0800

    Prepare next development cycle

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 kwargs = dict(
     name="bd2k-python-lib",
-    version="1.10.dev5",
+    version="1.10.dev6",
 
     author='Hannes Schmidt',
     author_email='hannes@ucsc.edu',
