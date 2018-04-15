8a971f946c5ab75c0569d7ee028c914568377522
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-04-15 07:56:12 -0700

    add py3 to jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -3,3 +3,9 @@ virtualenv venv
 make develop
 export PYTEST_ADDOPTS="--junitxml=test-report.xml"
 make $make_targets
+
+virtualenv -p python3 venv3
+. venv3/bin/activate
+make python=python3 develop
+export PYTEST_ADDOPTS="--junitxml=test-report.xml"
+make python=python3 $make_targets
